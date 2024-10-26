6aa5d939651fcb2c342d8aaa8da5048bfc1d1f7b
TeensForLife <githubalt.cake077@silomails.com>
2024-10-26 16:00:09 +1000

    Add username filtering, change to kick to timeout

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ def load_db():
     cursor = conn.cursor()
 
     cursor.execute('''
-    CREATE TABLE IF NOT EXISTS ageRequirements (
+    CREATE TABLE IF NOT EXISTS kickExceptions (
         userid INTEGER PRIMARY KEY
     )
     ''')
@@ -48,10 +48,10 @@ def load_db():
     conn.close()
 
 
-async def query_age_requirements(id):
+async def query_kick_exception(id):
     conn = await aiosqlite.connect('users.db')
     cursor = await conn.cursor()
-    await cursor.execute('SELECT userid FROM ageRequirements WHERE userid = ?', (id,))
+    await cursor.execute('SELECT userid FROM kickExceptions WHERE userid = ?', (id,))
     result = await cursor.fetchone()
     await conn.close()
 
@@ -77,20 +77,46 @@ async def on_command_error(ctx, error):
 
 @bot.event
 async def on_member_join(member):
-    minimum_age = configuration['min_age']
-    account_created = member.created_at
-    now = datetime.now(timezone.utc)
-    delta = now - account_created
+    report_channel = bot.get_channel(configuration['reports_channel'])
+    minimum_age = timedelta(days=configuration['min_age'])
+    restricted_words = {
+        'gay': 0.5,
+        'cp': 1,
+        'sell': 1,
+        'rape': 2,
+        'incest': 2,
+        'mega': 0.5,
+        'link': 0.5,
+        'legit': 0.5
+    }
 
-    is_allowed = await query_age_requirements(member.id)
+    # check if user is an exception
+    is_allowed = await query_kick_exception(member.id)
     if is_allowed is not None:
         return
 
-    if delta <= timedelta(days=minimum_age):
-        await member.send(f'Your account does not meet the minimum creation date requirements. Please check back in {minimum_age - delta.days} days! :smile:')
-        await member.kick(reason="Account doesn't meet minimum creation date requirements")
-        channel = bot.get_channel(configuration['reports_channel'])
-        await channel.send(f'Kicked new user for account age: {member.mention}')
+    # check username
+    username = member.name
+    username_score = 0
+    for word in restricted_words.keys():
+        if word in username:
+            username_score += restricted_words[word]
+    if username_score >= 2:
+        await member.send('Your username contains words that are not allowed on our this server. You have been banned :frowning:')
+        await member.ban(delete_message_days=1, reason='Username contains flagged words')
+        await report_channel.send(f'Banned new user for flagged username: {member.mention}')
+        return
+
+    # check account creation date
+    account_created = member.created_at
+    now = datetime.now(timezone.utc)
+    delta = now - account_created
+    timeout_until = minimum_age - delta
+    if delta <= minimum_age:
+        await member.send(f'Your account does not meet the minimum creation date requirements; therefore, you have been timed out. Please check back in {timeout_until.days} days {round(timeout_until.seconds/3600, 2)} hours! :smile:')
+        await member.timeout(timeout_until, reason="Account doesn't meet minimum creation date requirements")
+        await report_channel.send(f'Timed out new user for account age: {member.mention}')
+        return
 
 
 @bot.command(help='Set minimum account age for new members. Counts in days from present time (arg1: integer<days>)')
@@ -108,8 +134,8 @@ async def set_min_creation_date(ctx, arg):
         json.dump(configuration, conf)
 
 
-@bot.command(help='Add exclusion to account age restriction based on user ID (arg1: integer<ID>')
-@commands.has_permissions(kick_members=True)
+@bot.command(help='Add exclusion to account auto-filter based on user ID (arg1: integer<ID>')
+@commands.has_permissions(ban_members=True)
 async def allow_user_age(ctx, arg):
     conn = await aiosqlite.connect('users.db')
     cursor = await conn.cursor()
@@ -117,7 +143,7 @@ async def allow_user_age(ctx, arg):
     try:
         user_id = int(arg)
         username = await bot.fetch_user(user_id)
-        await cursor.execute('SELECT userid FROM ageRequirements WHERE userid = ?', (user_id,))
+        await cursor.execute('SELECT userid FROM kickExceptions WHERE userid = ?', (user_id,))
         valid = await cursor.fetchone()
         if valid:
             await ctx.reply(f"User '{username}' already excluded")
@@ -126,14 +152,14 @@ async def allow_user_age(ctx, arg):
         await ctx.reply('Please pass valid user ID')
         return
 
-    await cursor.execute('INSERT INTO ageRequirements (userid) VALUES (?)', (user_id,))
+    await cursor.execute('INSERT INTO kickExceptions (userid) VALUES (?)', (user_id,))
     await conn.commit()
     await conn.close()
     await ctx.reply(f"Successfully added user '{username}' to exception list")
 
 
-@bot.command(help='Remove existing exclusion to account age restriction based on user ID (arg1: integer<ID>')
-@commands.has_permissions(kick_members=True)
+@bot.command(help='Remove existing exclusion to account auto-filter based on user ID (arg1: integer<ID>')
+@commands.has_permissions(ban_members=True)
 async def deny_user_age(ctx, arg):
     conn = await aiosqlite.connect('users.db')
     cursor = await conn.cursor()
@@ -141,7 +167,7 @@ async def deny_user_age(ctx, arg):
     try:
         user_id = int(arg)
         username = await bot.fetch_user(user_id)
-        await cursor.execute('SELECT userid FROM ageRequirements WHERE userid = ?', (user_id,))
+        await cursor.execute('SELECT userid FROM kickExceptions WHERE userid = ?', (user_id,))
         valid = await cursor.fetchone()
         if not valid:
             await ctx.reply('User ID not in table, ignoring')
@@ -150,7 +176,7 @@ async def deny_user_age(ctx, arg):
         await ctx.reply('Please pass valid user ID')
         return
 
-    await cursor.execute('DELETE FROM ageRequirements WHERE userid = ?', (user_id,))
+    await cursor.execute('DELETE FROM kickExceptions WHERE userid = ?', (user_id,))
     await conn.commit()
     await conn.close()
 
